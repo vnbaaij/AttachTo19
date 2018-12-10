484552c94657ed290c684277e2ef6c739ea431d3
Vincent Baaij <vnbaaij@outlook.com>
2018-12-10 11:57:51 +0100

    Update license etc.

--- AttachTo19/Properties/AssemblyInfo.cs
+++ AttachTo19/Properties/AssemblyInfo.cs
@@ -2,32 +2,32 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AttachTo17")]
+[assembly: AssemblyTitle("AttachTo19")]
 [assembly: AssemblyDescription("Visual Studio 2019 Extension adding Attach to IIS, IISExpress and NUnit commands")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vincent Baaij")]
 [assembly: AssemblyProduct("AttachTo19")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
